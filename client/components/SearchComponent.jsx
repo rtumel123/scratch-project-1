8312a2a47a2a405064ad286bc9b31765a729a367
Ryan Tumel <rtumel123@gmail.com>
2020-09-16 15:35:32 -0400

    link frontend to backend

--- client/components/SearchComponent.jsx
+++ client/components/SearchComponent.jsx
@@ -13,7 +13,7 @@ class SearchComponent extends Component {
   checkNow() {
     return axios
       .post(
-        "http://localhost:3000/",
+        "http://localhost:3000/api/searchRecipes",
         {
           name: this.props.name,
         }
@@ -23,7 +23,7 @@ class SearchComponent extends Component {
           this.props.handleClick({
             ingredients: recipe.ingredients,
             instructions: recipe.instructions,
-            imageLink: recipe.imageLink,
+            imagelink: recipe.imagelink,
           })
         })
       )
@@ -37,7 +37,7 @@ class SearchComponent extends Component {
         <input
         id = 'input'
         type = "text"
-        onChange = {e => props.dispatchRecipe(e.target.value)}
+        onChange = {e => this.props.dispatchRecipe(e.target.value)}
       ></input>
       <button onClick ={this.checkNow} >Search</button>
     </div>
